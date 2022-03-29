bb8aa74249327dcfb09a48f347309f905be480d9
huntsolo <40738042+huntsolo@users.noreply.github.com>
2022-03-29 21:48:33 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 apt update && apt install nginx -y
 cp ./default /etc/nginx/sites-available
-systemctl restart nginx mkdir /opt/keys
+systemctl restart nginx
 mkdir /opt/keys
 chown -R ubuntu:ubuntu /opt/keys
